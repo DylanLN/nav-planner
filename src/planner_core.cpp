f3c9ce48b4e37d5cb36150385ecee075bfdab557
DylanLN <825255961@qq.com>
2021-02-04 17:11:36 +0800

    rcgx

--- src/planner_core.cpp
+++ src/planner_core.cpp
@@ -68,6 +68,8 @@ GlobalPlanner::~GlobalPlanner()
 //初始化函数1
 void GlobalPlanner::initialize(std::string name, costmap_2d::Costmap2DROS* costmap_ros)
 {
+    //调用初始化函数2
+    //将 Costmap2DROS 转化位 costmap 提取其 frameid
     initialize(name, costmap_ros->getCostmap(), costmap_ros->getGlobalFrameID());
 }
 
@@ -85,13 +87,123 @@ void GlobalPlanner::initialize(std::string name, costmap_2d::Costmap2D* costmap,
         unsigned int cx = costmap->getSizeInCellX();
         unsigned int cy = costmap->getSizeInCellY();
 
-        private_nh.param();
+        //是否使用navfn的方式  no
+        convert_offset_ = 0.5;
+
+        //是否使用二次近似函数    no
+        p_calc_ = new Potential(cx, cy);
+
+        //使用地杰斯特拉算法 yes
+        DijkstraExpansion* de = new DijkstraExpansion(p_calc_, cx, cy);
+            //是否使用老旧的计算方式 no
+            de->setPreciseStart(true);
+        planner_ = de;
+
+        //延边界创建路径 yes  使用梯度下降 no
+        path_marker_ = new GridPath(p_calc_);
+
+        //给路径加方向
+        orientation_filter_ = new orientationFilter();
+
+        //发布消息
+        plan_pub_ = private_nh.advertise<nav_msgs::Path>("plan",1);
+        potential_pub_ = private_nh.advertise<nav_msgs::OccupancyGrid>("potential",1);
+
+        //允许规划 未知（unknow）中的全局路径 no
+        planner_->setHasUnknown(false);
+
+        //设置允许目标点误差范围
+        default_tolerance_ = 0.0；
+
+        //获取tf 的prefix(字首)
+        ros::NodeHandle prefix_nh;
+        tf_prefix = tf:: getPrefixParam(prefix_nh);
+
+        //发布make plan服务
+        make_plan_src_ = private_nh.advertiseService("make_plan", &GlobalPlanner::makePlanService, this);
+
+        //动态调参 略去
+
+        initialized_ = true;
+    }else{
+        //这个已经被初始化，您不能调用两次
+        ROS_WARN("This planner has already been initialized, you can't call it twice, doing nothing");
     }
-    
 }
 
+//将某个costmap设置为free
+void GlobalPlanner::clearRobotCell(const tf::Stamped<tf::Pose>& global_pose, unsigned int mx, unsigned int my)
+{
+    if (initialized_){
+        costmap->setCost(mx, my, costmap_2d::FREE_SPACE);
+    }else{
+        ROS_ERROR("this planner has not been initialized yet");
+    }
+}
 
+//makeplan 回调函数
+bool GlobalPlanner::makePlanService(nav_msgs::GetPlan::Request& req, nav_msgs::GetPlan::Response& res)
+{
+    //调用makeplan函数
+    makePlan(req.start, req.goal, res.plan.poses);
 
+    //赋予时间及frame id
+    res.plan.header.stamp = ros::Time::now();
+    res.plan.header.frame_id = frame_id_;
+
+    return true;
+}
+
+//将map上的坐标系转化为world上的坐标系
+void GlobalPlanner::mapToWorld(double mx, double my, double& wx, double& wy)
+{
+    wx = costmap_->getOriginX() + (mx + convert_offset_) * costmap_->getResolution();
+    wy = costmap_->getOriginY() + (my + convert_offset_) * cosrmap_->getResolution();
+}
+
+//将world上的坐标转化为map上的坐标
+bool GlobalPlanner::worldToMap(double wx, double wy, double& mx, double& my)
+{
+    //获得地图的长宽（栅格地图）
+    double origin_x = costmap_->getOriginX();
+    double origin_y = costmap_->getOriginY();
+
+    double resolution = costmap_->getResolution();
+
+    //如果超出范围
+    if (wx < origin_x || wy < origin_y)
+        return false;
+    // x = (当前点 - 长度)/分辨率  - 转换偏移
+    mx = (wx - origin_x) / resolution - convert_offset_;
+
+    my = (wy - origin_y) / resolution - convert_offset_;
+
+    if (mx < costmap_->getSizeInCellsX() && my < costmap_->getSizeInCellsY())
+        return true;
+
+    return false;
+}
+
+bool GlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start,
+                             const geometry_msgs::PoseStamped& goal,
+                             std::vector<geometry_msgs::PoseStamped>& plan)
+{
+    return makePlan(start, goal, default_tolerance_, plan);
+}
+
+//主要函数：路径规划函数
+//start 起始点的位资
+//goal 目标点的位资
+//telerance 目标点的允许误差
+//plan 路径
+bool GlobalPlanner::makePlan(const geometry_msgs::PoseStamped& start,
+                             const geometry_msgs::PoseStamped& goal,
+                             double tolerance,
+                             std::vector<geometry_msgs::PoseStamped>& plan)
+{
+    
+
+}
 
 }
 
